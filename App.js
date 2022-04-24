05864caa03abf4275ee5c5631271f96470a34b3c
Md Naushad Ahamed <99961853+naushadcom@users.noreply.github.com>
2022-04-24 20:18:06 +0530

    file uploaded

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import Todo from './Asignments-Todo/Class/Todo/Todo';
+import Resturent from './Resturents/Resturent';
+
+
+import Main from './Class/Todo/Main';
+
+// import Employ from './sprint-1/day-3/assignments/PaymentCard/Employ';
+// import Todo from './sprint-1/day-3/assignments/Todo/Todo';
+
+
+
+
+
+
+
+
+function App() {
+ 
+  return (
+    <div className="App">
+     {/* <ShowHide/> */}
+
+     {/* <Main/>
+     */}
+
+     <Resturent/>
+    </div>
+  );
+}
+
+export default App;
